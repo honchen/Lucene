b96a4dd44462adf56810e172ee93272efa2d50e3
honchen <525087452@qq.com>
2017-08-02 17:58:21 +0800

    Test

---
+++ src/test/java/com/shop/demo/CreatIndexTest.java
@@ -0,0 +1,27 @@
+//package com.shop.demo;
+//
+//import org.junit.FixMethodOrder;
+//import org.junit.Test;
+//import org.junit.runners.MethodSorters;
+//
+//import com.shop.demo.CreatIndex;
+//
+//
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+//public class CreatIndexTest {
+//
+//	private static CreatIndex creatIndex = new CreatIndex();
+//
+//	@SuppressWarnings("static-access")
+//	@Test
+//	public void testNewIndex() {
+//		creatIndex.newIndex("F:/luceneTest");
+//	}
+//
+//	@SuppressWarnings("static-access")
+//	@Test
+//	public void testSearcherIndex() {
+//		creatIndex.searcherIndex("lucene");
+//	}
+//
+//}
